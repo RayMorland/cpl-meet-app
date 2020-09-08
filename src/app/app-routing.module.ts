3343bb5c4a883cd025cb360f42d61b460c8f6eda
RayMorland <raymondmorland@gmail.com>
2020-09-07 22:19:29 -0700

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InfoComponent } from './info/info.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'info',
+    component: InfoComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'electron-app';
+  meets: any;
+  meetsReady = false;
+  constructor(
+    private http: HttpClient,
+    private changeDetector: ChangeDetectorRef
+  ){}
+
+  ngOnInit() {
+    this.getMeets().subscribe(res => {
+      this.meets = res;
+      console.log(this.meets);
+      this.meetsReady = true;
+      this.changeDetector.detectChanges();
+    });
+  }
+
+  public getMeets(): Observable<any> {
+    return this.http.get('http://localhost:3000/api/getmeets', {});
+  }
 }
